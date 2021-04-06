fcdf11755dd17c336ccc2404eaf0e7f27dd259c1
Cory Sougstad <cory@hau.me>
2021-04-05 19:37:46 -0700

    Better jump
    More dynamic jump and float

--- Assets/Scenes/Main Scene/Scripts/PlayerController.cs
+++ Assets/Scenes/Main Scene/Scripts/PlayerController.cs
@@ -25,6 +25,17 @@ public class PlayerController : MonoBehaviour
 
     public Camera playerCam;
 
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+    private enum PlayerState {
+        Jumping,
+        Floating,
+        Idle,
+    }
+
+    private PlayerState playerState = PlayerState.Idle;
+
     private void Awake()
     {
         rb = transform.GetComponent<Rigidbody2D>();
@@ -32,18 +43,50 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(playerSpeed, rb.velocity.y);
+    }
+
     private void Update()
     {
+
         if(IsGrounded() && Input.GetKeyDown(KeyCode.Space)){
-            rb.velocity = Vector2.up * jumpVelocity;    
+            playerState = PlayerState.Jumping;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpVelocity;
         }
-        rb.velocity = new Vector2(+playerSpeed, rb.velocity.y);
+
+        if(Input.GetKey(KeyCode.Space) && playerState == PlayerState.Jumping){
+            if(jumpTimeCounter > 0){
+                rb.velocity = Vector2.up * jumpVelocity;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                playerState = PlayerState.Idle;
+            }
+            
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space)){
+            playerState = PlayerState.Idle;
+        }
+
+        if(
+            !IsGrounded() && jumpTimeCounter > 0 &&
+            (playerState == PlayerState.Floating || playerState == PlayerState.Idle) && 
+            (Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.Space))
+        ){
+            rb.velocity = Vector2.up * 0;
+            jumpTimeCounter -= Time.deltaTime;
+            playerState = PlayerState.Floating;
+        }   
+
+
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 1f, platformLayermask);
-        //Debug.Log(raycast.collider);
+        RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.01f, platformLayermask);
+        Debug.Log(raycast.collider);
         return raycast.collider != null;
     }
 
